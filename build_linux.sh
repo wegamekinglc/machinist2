b84a13a37d4156b69d5abb34a1ba5db7242e8d34
wegamekinglc <scrappedprince.li@gmail.com>
2020-10-27 15:07:49 +0800

    FEATURE: added build script

---
+++ build_linux.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export num_cores=`grep -c processor /proc/cpuinfo`
+export MACHINIST_DIR=$PWD
+export BUILD_TYPE=Debug
+
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$DAL_DIR ..
+make clean
+make -j${num_cores}
+make install
+
+cd ..
+echo "Finished building of Machinist"
