686001d29eb5fee3ea47b92653f0e1733566539d
wegamekinglc <scrappedprince.li@gmail.com>
2020-11-09 18:16:04 +0800

    FIX: added abi lib for clang

--- build_linux.sh
+++ build_linux.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
-export num_cores=`grep -c processor /proc/cpuinfo`
+export num_cores=$(grep -c processor /proc/cpuinfo)
 export MACHINIST_DIR=$PWD
 export BUILD_TYPE=Debug
 
 mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$DAL_DIR ..
-make clean
-make -j${num_cores}
-make install
-
-cd ..
+(
+  cd build || exit
+  cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$DAL_DIR ..
+  make clean
+  make -j${num_cores}
+  make install
+)
 echo "Finished building of Machinist"
