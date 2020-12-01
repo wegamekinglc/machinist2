9f6a2ebe194bfc9d6ea1d268848b3e547ff20010
wegamekinglc <scrappedprince.li@gmail.com>
2020-12-02 02:57:33 +0800

    FIX: update windows build script

--- build_windows.bat
+++ build_windows.bat
@@ -1,10 +1,14 @@
-@echo off
+call :set_variable BUILD_TYPE Release %BUILD_TYPE%
+call :set_variable ADDRESS_MODEL Win64 %ADDRESS_MODEL%
+call :set_variable MACHINIST_DIR "%CD%" %MACHINIST_DIR%
+call :set_variable MSVC_RUNTIME dynamic %MSVC_RUNTIME%
+call :set_variable MSVC_VERSION "Visual Studio 16 2019" %MSVC_VERSION%
 
-call :set_variable BUILD_TYPE Release
-call :set_variable DAL_DIR "%CD%"
-call :set_variable ADDRESS_MODEL
-call :set_variable MSVC_RUNTIME dynamic
-call :set_variable MSVC_VERSION Visual Studio 15 2017
+echo BUILD_TYPE:  %BUILD_TYPE%
+echo MACHINIST_DIR: %MACHINIST_DIR%
+echo ADDRESS_MODEL: %ADDRESS_MODEL%
+echo MSVC_RUNTIME: %MSVC_RUNTIME%
+echo MSVC_VERSION: %MSVC_VERSION%
 
 if exist build (
   rem build folder already exists.
@@ -15,21 +19,26 @@ if exist build (
 cd build
 
 if "%ADDRESS_MODEL%"=="Win64" (
+  set PLATFORM=x64
+) else (
+  if "%MSVC_VERSION%"=="Visual Studio 16 2019" (
+    set PLATFORM=x64
+  ) else (
+    set PLATFORM=Win32
+  )
+)
+
+if "%ADDRESS_MODEL%"=="Win64" (
   if "%MSVC_VERSION%"=="Visual Studio 16 2019" (
     set ADDRESS_MODEL=
   )
 )
 
-if "%ADDRESS_MODEL%"=="Win64" (
-  set PLATFORM=x64
-) else (
-  set PLATFORM=Win32
-)
-
 if "%ADDRESS_MODEL%" =="Win64" (
-cmake -G "%MSVC_VERSION% %ADDRESS_MODEL%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%DAL_DIR% -DMSVC_RUNTIME=%MSVC_RUNTIME% --target install ..
+cmake -G "%MSVC_VERSION% %ADDRESS_MODEL%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%MACHINIST_DIR% -DMSVC_RUNTIME=%MSVC_RUNTIME% --target install ..
+echo go into this branch
 ) else (
-cmake -G "%MSVC_VERSION%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%DAL_DIR% -DMSVC_RUNTIME=%MSVC_RUNTIME% --target install ..
+cmake -G "%MSVC_VERSION%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%MACHINIST_DIR% -DMSVC_RUNTIME=%MSVC_RUNTIME% --target install ..
 )
 
 if %errorlevel% neq 0 exit /b 1
@@ -41,11 +50,8 @@ if %errorlevel% neq 0 exit /b 1
 
 cd ..
 
-@echo on
-
 :set_variable
-if "%~1%"=="" (
- set ~1=~2
+if "%~3"=="" (
+ set %~1=%~2
 )
-echo %~1 is %~2
 EXIT /B 0
