8cb66ef22cf684ee73fb2d7c453081b387029165
wegamekinglc <scrappedprince.li@gmail.com>
2021-09-25 16:14:24 +0800

    mkdir with -p

--- build_linux.sh
+++ build_linux.sh
@@ -4,7 +4,7 @@ export num_cores=$(grep -c processor /proc/cpuinfo)
 export MACHINIST_DIR=$PWD
 export BUILD_TYPE=Debug
 
-mkdir build
+mkdir -p build
 (
   cd build || exit
   cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$DAL_DIR ..
