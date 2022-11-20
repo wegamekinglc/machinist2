19c905ac3a4805ef6f787be9657684b67e398d8a
wegamekinglc <scrappedprince.li@gmail.com>
2022-11-20 21:55:39 +0800

    using static on windows

--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,7 @@
 call :set_variable BUILD_TYPE Release %BUILD_TYPE%
 call :set_variable ADDRESS_MODEL Win64 %ADDRESS_MODEL%
 call :set_variable MACHINIST_DIR "%CD%" %MACHINIST_DIR%
-call :set_variable MSVC_RUNTIME dynamic %MSVC_RUNTIME%
+call :set_variable MSVC_RUNTIME static %MSVC_RUNTIME%
 call :set_variable MSVC_VERSION "Visual Studio 17 2022" %MSVC_VERSION%
 
 echo BUILD_TYPE:  %BUILD_TYPE%
